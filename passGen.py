eb65027c3c4d9d363f8426476c111e96d77073c4
Diego Ramirez Caballero <diego.fer659@gmail.com>
2020-08-09 11:11:11 -0500

    remove unnecesary variables from passGen file.

--- passGen.py
+++ passGen.py
@@ -4,10 +4,8 @@ from string import ascii_letters, digits
 
 def new_pass(paswlength):
     """Function that generates random password based on user given length."""
-    letters = ascii_letters
-    numbers = digits
     special = '#$%&()*+<=>?@_'
-    lists = [letters, numbers, special]
+    lists = [ascii_letters, digits, special]
 
     pasw = ''.join(choice(choices(lists)[0]) for i in range(paswlength))
     print("generated password: ", pasw)
